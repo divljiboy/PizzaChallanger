ff6394908015795ea1e4c578846d17c28f2baf88
divljiboy <ivan93.ns@hotmail.com>
2017-04-11 00:00:02 +0200

    Small change to constructor

---
+++ Rendered/src/Rendered/POCO/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+public class Pizza
+{
+    public string name { get; set; }
+    public double price { get; set; }
+    public List<string> ingredients { get; set; }
+
+
+    public Pizza(string _name,double _price,List<string> _ingredients) {
+
+        name = _name;
+        price = _price;
+        ingredients = _ingredients;
+        
+    }
+    public Pizza() { 
+
+    }
+    
+}
